a067236ca0031d1c627d0b49df472a4e365be192
Nelamin2 <nidalelamin@gmail.com>
2024-04-20 05:27:23 +0300

    tasks

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -36,3 +36,7 @@ def pythoniscool(text='is cool'):
 def imanumber(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
